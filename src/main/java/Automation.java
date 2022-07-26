96efbb2586dc09c35c8d0790e86935591dcb727a
shivam11-01 <91551726+shivam11-01@users.noreply.github.com>
2022-07-26 23:29:57 +0530

    Update Automation.java

--- src/main/java/Automation.java
+++ src/main/java/Automation.java
@@ -15,6 +15,7 @@ import java.time.Duration;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import java.io.*;
+import org.openqa.selenium.JavascriptExecutor;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.usermodel.HSSFRow;
@@ -23,9 +24,11 @@ public class Automation {
     public static WebDriver driver;
     public static Duration waitDuration = Duration.ofSeconds(120);
     public static WebDriverWait wait;
-    public static String chromeD = "C:\\chromedriver.exe";
+    
+    public static String chromeD = "C:\\Users\\" + System.getProperty("user.name") + "\\Desktop\\chromedriver.exe";
     public static long pending_for_processing;
     static Logger log = Logger.getLogger(Automation.class);
+    public static JavascriptExecutor js;
 
     public static void setLog(String msg) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
@@ -57,6 +60,8 @@ public class Automation {
         wait = new WebDriverWait(driver, waitDuration);
 
         driver.get("https://annalectindia.darwinbox.in");
+        
+        js = (JavascriptExecutor) driver;
 
         driver.manage().window().maximize();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
@@ -81,7 +86,7 @@ public class Automation {
         Thread.sleep(20000);
         driver.findElement(By.xpath("//*[@id=\"form61\"]/div[2]")).click();
         setLog("Okta Code Verified!");
-        Thread.sleep(20000);
+        Thread.sleep(15000);
 
 //        boolean submitbuttonPresence = driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/div/form/div/div/div/div[3]/button[1]")).isDisplayed();
 //        if(submitbuttonPresence) {
@@ -89,6 +94,11 @@ public class Automation {
 //            setLog("Modal Closed!");
 //            Thread.sleep(1500);
 //        }
+        
+//        /html/body/div[1]/div/div/div[1]/header/div[1]/div[2]/a
+        
+        driver.get("https://annalectindia.darwinbox.in");
+        Thread.sleep(5000);
         driver.findElement(By.tagName("body")).sendKeys(Keys.END);
         Thread.sleep(5000);
         driver.findElement(By.xpath("/html/body/div[2]/div/nav/div[1]/div[2]/div[2]/div/a")).click();
@@ -97,7 +107,7 @@ public class Automation {
     }
 
     public static void pending_and_processing() throws Exception{
-//        Thread.sleep(2000);
+//        Thread.sleep(2000);`
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div/section/div/div[4]/div[1]/ul/li[2]/a"))).click();
         setLog("Selecting Pending for processing Tab");
         Thread.sleep(10000);
@@ -124,7 +134,7 @@ public class Automation {
 //          Thread.sleep(1000);
 
 //        Selecting the month i.e. July Month
-        driver.findElement(By.xpath("/html/body/div[3]/div/div/select[1]/option[1]")).click();
+        driver.findElement(By.xpath("/html/body/div[3]/div/div/select[1]/option[6]")).click();
 
 //          Thread.sleep(1000);
 
@@ -142,7 +152,15 @@ public class Automation {
         //          Thread.sleep(1000);
 
 //        Selecting the date i.e. 01
-        driver.findElement(By.xpath("/html/body/div[3]/table/tbody/tr[1]/td[7]")).click();
+        driver.findElement(By.xpath("/html/body/div[3]/table/tbody/tr[2]/td[4]/a")).click();
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ReimbAdminFilters_created_to_date"))).click();
+//        
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div/div/select[1]"))).click();
+//        
+//        driver.findElement(By.xpath("/html/body/div[3]/div/div/select[1]/option[7]")).click();
+//        
+//        driver.findElement(By.xpath("/html/body/div[3]/table/tbody/tr[2]/td[5]/a")).click();
+
 //          Thread.sleep(1000);
         setLog("Selected date for Created Date filter");
 
@@ -152,7 +170,13 @@ public class Automation {
     }
 
     public static void download_pending_processes() throws Exception {
+    	
+    	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div/section/div/div[4]/form/div[1]/div/div/table/thead/tr[1]/th[6]/a"))).click();  	
+    	Thread.sleep(3000);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div/section/div/div[2]/form/button")));
+        
+        
+        
         WebElement p = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div/section/div/div[4]/div[1]/ul/li[2]/a/span")));
         String process = p.getText();
         pending_for_processing = Integer.parseInt(process);
@@ -161,6 +185,7 @@ public class Automation {
         System.out.println(pending_for_processing);
 
         int k = 2;
+        int scroll = 0;
         for(int i = 1; i <= pending_for_processing; i++) {
             String string_id = "/html/body/div[2]/div/section/div/div[4]/form/div[1]/div/div/table/tbody/tr[" + i +"]/td[2]";
             String string_name = "/html/body/div[2]/div/section/div/div[4]/form/div[1]/div/div/table/tbody/tr[" + i + "]/td[5]/a[2]";
@@ -218,9 +243,10 @@ public class Automation {
             Thread.sleep(1000);
             driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-            driver.findElement(By.xpath("/html/body/div[2]/div/section/div/div[4]/form/div[1]/div/div/table/tbody/tr[" + i + "]/td[11]/div/ul/li[1]/a")).click();
+            driver.findElement(By.xpath("/html/body/div[2]/div/section/div/div[4]/form/div[1]/div/div/table/tbody/tr[" + i + "]/td[11]/div/div/ul/li[1]/a")).click();
             setLog("PDF Downloaded");
             Thread.sleep(5000);
+
 
 
             try {
@@ -252,11 +278,12 @@ public class Automation {
                     }
                 }
 
-                driver.findElement(By.xpath("/html/body/div[2]/div/section/div/div[4]/form/div[1]/div/div/table/tbody/tr["+ i +"]/td[11]/div/div/button")).click();
-                Thread.sleep(1000);
+                driver.findElement(By.xpath("/html/body/div[2]/div/section/div/div[4]/form/div[1]/div/div/table/tbody/tr[" + i +"]/td[11]/div/div/button")).click();
+                Thread.sleep(2000);
                 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-
-                driver.findElement(By.xpath("/html/body/div[2]/div/section/div/div[4]/form/div[1]/div/div/table/tbody/tr["+ i +"]/td[11]/div/ul/li[2]/a")).click();
+                
+                driver.findElement(By.xpath("/html/body/div[2]/div/section/div/div[4]/form/div[1]/div/div/table/tbody/tr[" + i + "]/td[11]/div/div/ul/li[2]/a")).click();
+                Thread.sleep(2000);
                 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
                 setLog("ZIP Downloaded");
                 Thread.sleep(3000);
@@ -292,6 +319,11 @@ public class Automation {
                 setLog("Error moving files: " + e.getMessage());
                 System.err.println(e.getMessage());
             }
+            
+            if(i >= 3) {
+            	js.executeScript("window.scrollBy(0,100)", "");
+            	Thread.sleep(5000);
+            }
             if(i % 10 == 0) {
                 if(k > 6) {
                     wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div/section/div/div[4]/form/div[1]/div/div/table/tfoot/tr/td/a[6]"))).click();
@@ -316,61 +348,61 @@ public class Automation {
         log.info("********************** Exited **********************");
     }
 
-    public static void excelReport(String empID, String name, String expID, String claimTitle, String claimBy, String claimDate, String expAmount, String appAmount, String approvedBy, String approvedOn) throws IOException {
-        try
-        {
-            String filename = "src\\report\\example.xls";
-            HSSFWorkbook workbook = new HSSFWorkbook();
-            HSSFSheet sheet = workbook.createSheet("January");
-            HSSFRow rowhead = sheet.createRow((short)0);
-
-            rowhead.createCell(0).setCellValue("Employee ID");
-            rowhead.createCell(1).setCellValue("Employee Name");
-            rowhead.createCell(2).setCellValue("Expense ID");
-            rowhead.createCell(3).setCellValue("Claim Title");
-            rowhead.createCell(4).setCellValue("Claim By");
-            rowhead.createCell(5).setCellValue("Claim Date");
-            rowhead.createCell(6).setCellValue("Expense Amount");
-            rowhead.createCell(7).setCellValue("Approved Amount");
-            rowhead.createCell(8).setCellValue("Approved By");
-            rowhead.createCell(9).setCellValue("Approved On");
-
-            for(int j = 1; j < pending_for_processing; j++) {
-                HSSFRow row = sheet.createRow((short)j);
-                int k = 0;
-                row.createCell(k).setCellValue(empID);
-                k++;
-                row.createCell(k).setCellValue(name);
-                k++;
-                row.createCell(k).setCellValue(expID);
-                k++;
-                row.createCell(k).setCellValue(claimTitle);
-                k++;
-                row.createCell(k).setCellValue(claimBy);
-                k++;
-                row.createCell(k).setCellValue(claimDate);
-                k++;
-                row.createCell(k).setCellValue(expAmount);
-                k++;
-                row.createCell(k).setCellValue(appAmount);
-                k++;
-                row.createCell(k).setCellValue(approvedBy);
-                k++;
-                row.createCell(k).setCellValue(approvedOn);
-            }
-            FileOutputStream fileOut = new FileOutputStream(filename);
-            workbook.write(fileOut);
-            fileOut.close();
-            workbook.close();
-            setLog("Data Added to report successfully");
-            System.out.println("Data Added to report successfully");
-        }
-        catch (Exception e)
-        {
-            setLog("Error: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
+//    public static void excelReport(String empID, String name, String expID, String claimTitle, String claimBy, String claimDate, String expAmount, String appAmount, String approvedBy, String approvedOn) throws IOException {
+//        try
+//        {
+//            String filename = "src\\report\\example.xls";
+//            HSSFWorkbook workbook = new HSSFWorkbook();
+//            HSSFSheet sheet = workbook.createSheet("January");
+//            HSSFRow rowhead = sheet.createRow((short)0);
+//
+//            rowhead.createCell(0).setCellValue("Employee ID");
+//            rowhead.createCell(1).setCellValue("Employee Name");
+//            rowhead.createCell(2).setCellValue("Expense ID");
+//            rowhead.createCell(3).setCellValue("Claim Title");
+//            rowhead.createCell(4).setCellValue("Claim By");
+//            rowhead.createCell(5).setCellValue("Claim Date");
+//            rowhead.createCell(6).setCellValue("Expense Amount");
+//            rowhead.createCell(7).setCellValue("Approved Amount");
+//            rowhead.createCell(8).setCellValue("Approved By");
+//            rowhead.createCell(9).setCellValue("Approved On");
+//
+//            for(int j = 1; j < pending_for_processing; j++) {
+//                HSSFRow row = sheet.createRow((short)j);
+//                int k = 0;
+//                row.createCell(k).setCellValue(empID);
+//                k++;
+//                row.createCell(k).setCellValue(name);
+//                k++;
+//                row.createCell(k).setCellValue(expID);
+//                k++;
+//                row.createCell(k).setCellValue(claimTitle);
+//                k++;
+//                row.createCell(k).setCellValue(claimBy);
+//                k++;
+//                row.createCell(k).setCellValue(claimDate);
+//                k++;
+//                row.createCell(k).setCellValue(expAmount);
+//                k++;
+//                row.createCell(k).setCellValue(appAmount);
+//                k++;
+//                row.createCell(k).setCellValue(approvedBy);
+//                k++;
+//                row.createCell(k).setCellValue(approvedOn);
+//            }
+//            FileOutputStream fileOut = new FileOutputStream(filename);
+//            workbook.write(fileOut);
+//            fileOut.close();
+//            workbook.close();
+//            setLog("Data Added to report successfully");
+//            System.out.println("Data Added to report successfully");
+//        }
+//        catch (Exception e)
+//        {
+//            setLog("Error: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
 
     public static String getEmpId(String name) {
         String[] one = name.split("\\(");
